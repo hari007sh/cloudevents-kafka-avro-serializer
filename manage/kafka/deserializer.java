66e81640503d2c74b3a31f490f6aeef3815add40
Harish Talluri <harishtalluri123@gmail.com>
2023-11-22 09:28:28 +0000

    ewrwe

--- manage/kafka/deserializer.java
+++ manage/kafka/deserializer.java
@@ -6,6 +6,7 @@ import io.cloudevents.core.format.EventDeserializationException;
 import io.cloudevents.kafka.impl.KafkaHeaders;
 import io.confluent.kafka.serializers.KafkaAvroDeserializer;
 import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.util.Utf8;
 import org.apache.kafka.common.header.Headers;
@@ -17,8 +18,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.nio.ByteBuffer;
 import java.util.*;
-
-import static java.lang.Class.forName;
 
 /**
  * Implementation of {@link Deserializer} parsing a {@link CloudEvent} and returning the payload.
@@ -68,14 +67,19 @@ public class CloudEventDeserializer<T> implements Deserializer<T> {
 
         Class<T> targetClass = (Class<T>) Class.forName(className);
         T targetObject = targetClass.newInstance();
-        for(Schema.Field field : record.getSchema().getFields()) {
+        for (Schema.Field field : record.getSchema().getFields()) {
             String fieldName = field.name();
             Object value = extractValue(record, fieldName);
+            boolean isMapHandled = false;
             if (value instanceof HashMap && targetClass.getDeclaredField(fieldName).getType().equals(Map.class)) {
-                value = handleMapAttributes((Map<?,?>) value, fieldName, targetObject,targetClass);
+                value = handleMapAttributes((Map<?, ?>) value, fieldName, targetObject, targetClass);
+                isMapHandled = true;
+            } else if (value instanceof List<?> || value instanceof GenericRecord) {
+                isMapHandled = true;
             }
-            invokeSetterMethod(targetClass, targetObject, fieldName, value);
-
+            if (!isMapHandled) {
+                invokeSetterMethod(targetClass, targetObject, fieldName, value);
+            }
         }
         return targetObject;
     }
@@ -84,8 +88,8 @@ public class CloudEventDeserializer<T> implements Deserializer<T> {
     private Object extractValue(GenericRecord record, String fieldName) {
         Object value = record.get(fieldName);
         if (value instanceof Utf8) {
-           return value.toString();
-        } else if (value instanceof List<?>) {
+            return value.toString();
+        } else if (value instanceof GenericData.Array || value instanceof List<?>) {
             return processListValues((List<?>) value);
         } else if (value instanceof GenericRecord) {
             return deserializeComplexObject((GenericRecord) value);
@@ -99,14 +103,32 @@ public class CloudEventDeserializer<T> implements Deserializer<T> {
         return value;
     }
 
+    private List<Object> processGenericDataArray(GenericData.Array<?> array) {
+        List<Object> processedList = new ArrayList<>();
+        for (Object item : array) {
+            if (item instanceof GenericRecord) {
+                processedList.add(deserializeComplexObject((GenericRecord) item));
+            } else {
+                processedList.add(item);
+            }
+        }
+        return processedList;
+    }
+
     private void invokeSetterMethod(Class<T> targetClass, T targetObject, String fieldName, Object value) {
         if (value == null) return;
         String setterName = "set" + Character.toUpperCase(fieldName.charAt(0)) + fieldName.substring(1);
         try {
             Method setter = targetClass.getMethod(setterName, value.getClass());
             setter.invoke(targetObject, value);
-        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-            System.err.println("No such Method found in Avro generated Class");
+        } catch (NoSuchMethodException e) {
+            System.err.println("No setter found for property: " + fieldName + "with method name: " + setterName);
+            //Handle exception or log it.
+        } catch (IllegalAccessException e) {
+            System.err.println("Unable to access setter for property: " + fieldName + ". Ensure the method is public.");
+        } catch (InvocationTargetException e) {
+            System.err.println("Setter for property: " + fieldName + "threw an exception.");
+            e.getTargetException().printStackTrace();
         }
     }
 
@@ -116,22 +138,31 @@ public class CloudEventDeserializer<T> implements Deserializer<T> {
         try {
             Class<?> targetClass = Class.forName(className);
             Object targetObject = targetClass.newInstance();
-            for(Schema.Field field : ((GenericRecord) genericRecordObject).getSchema().getFields()) {
+            for (Schema.Field field : ((GenericRecord) genericRecordObject).getSchema().getFields()) {
                 String fieldName = field.name();
-                Object value = ((GenericRecord) genericRecordObject).get(fieldName);
+                Object value = extractValue(genericRecordObject, fieldName);
                 if (value instanceof Utf8) {
                     value = value.toString();
-                }
-                else if (value instanceof Map<?,?>) {
-                    value = handleMapAttributes((Map<?,?>) value, fieldName, targetObject,targetClass);
+                } else if (value instanceof Map<?, ?>) {
+                    value = handleMapAttributes((Map<?, ?>) value, fieldName, targetObject, targetClass);
                 }
                 String setterName = "set" + Character.toUpperCase(fieldName.charAt(0)) + fieldName.substring(1);
                 if (value != null) {
                     try {
-                        Method setter = targetClass.getMethod(setterName, value.getClass());
+                        Method setter = null;
+                        if (value instanceof List) {
+                            setter = findListSetter(targetClass, setterName);
+                        } else {
+                            setter = targetClass.getMethod(setterName, value.getClass());
+                        }
                         setter.invoke(targetObject, value);
-                    } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-                        //Handle exception or log it.
+                    } catch (NoSuchMethodException e) {
+                        System.err.println("No setter found for property: " + fieldName + "with method name: " + setterName);
+                    } catch (IllegalAccessException e) {
+                        System.err.println("Unable to access setter for property: " + fieldName + ". Ensure the method is public.");
+                    } catch (InvocationTargetException e) {
+                        System.err.println("Setter for property: " + fieldName + "threw an exception.");
+                        e.getTargetException().printStackTrace();
                     }
                 }
             }
@@ -142,9 +173,19 @@ public class CloudEventDeserializer<T> implements Deserializer<T> {
         }
     }
 
-    private Object handleMapAttributes(Map<?,?> originalMap, String fieldName, Object targetObject, Class<?> targetClass) {
+    private Method findListSetter(Class<?> targetClass, String setterName) throws NoSuchMethodException{
+        for (Method method : targetClass.getMethods()) {
+            if(method.getName().equals(setterName) && List.class.isAssignableFrom(method.getParameterTypes()[0])) {
+                return method;
+            }
+
+        }
+        throw new NoSuchMethodException("setter method not found for list: " +setterName);
+    }
+
+    private Object handleMapAttributes(Map<?, ?> originalMap, String fieldName, Object targetObject, Class<?> targetClass) {
         Map<String, Object> processedMap = new HashMap<>();
-        for (Map.Entry<?,?> entry : originalMap.entrySet()) {
+        for (Map.Entry<?, ?> entry : originalMap.entrySet()) {
             Object mapValue = entry.getValue();
             processedMap.put(entry.getKey().toString(), mapValue);
         }
@@ -152,8 +193,13 @@ public class CloudEventDeserializer<T> implements Deserializer<T> {
         try {
             Method setter = targetClass.getMethod(setterName, Map.class);
             setter.invoke(targetObject, processedMap);
-        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-            //Handle exception or log it.
+        } catch (NoSuchMethodException e) {
+            System.err.println("No setter found for property: " + fieldName + "with method name: " + setterName);
+        } catch (IllegalAccessException e) {
+            System.err.println("Unable to access setter for property: " + fieldName + ". Ensure the method is public.");
+        } catch (InvocationTargetException e) {
+            System.err.println("Setter for property: " + fieldName + "threw an exception.");
+            e.getTargetException().printStackTrace();
         }
         return processedMap;
     }
